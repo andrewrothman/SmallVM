a8f00cc1cb3bf182dbbba0a9074e01c92124f710
Xavier Pinho <xavier.pinho.g@gmail.com>
2014-04-20 17:02:05 +0100

    Refactored header files

--- src/config.h
+++ src/config.h
@@ -1,3 +1,14 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <stdint.h> /* int32_t */
+
+#define REGISTER_COUNT 5 /* The number of registers */
+#define MEMORY_WORD_COUNT 20 /* The number of memory words */
+
+/* A word is just a 32 bit integer */
+typedef int32_t word;
+
 #define OP_SET     0x01 /* Sets a register to a value or to the contents of another register */
 #define OP_ADD     0x02 /* Adds two values or register contents */
 #define OP_SUB     0x03 /* Subtracts two values or register contents */
@@ -9,3 +20,5 @@
 #define OP_JMP     0x09 /* Jump to another location in memory */
 #define OP_IF      0x0A /* Performs the next instruction if the values are equal */
 #define OP_IFN     0x0B /* Performs the next instruction if the values are not equal */
+
+#endif /* CONFIG_H */
--- src/vm.c
+++ src/vm.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 #include "vm.h"
 
 vm_state *vm_new(void)
